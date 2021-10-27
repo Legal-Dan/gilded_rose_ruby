96cf062987f99db333e859d5ea76232537216e6c
Daniel Clements <clemies2000@hotmail.com>
2021-10-27 15:35:30 +0100

    Added more tests for the existing build to find the first point of failure

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -36,6 +36,67 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 1
     end
+
+    it "quality degrades twice as fast after the sell by date" do
+      items = [Item.new("foo", 0, 4)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 2
+    end
+
+    it "cannot have a negative quality value" do
+      items = [Item.new("foo", 1, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+    end
+
+    it "increases the quality value for Aged Brie" do
+      items = [Item.new("Aged Brie", 1, 2)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 3
+    end
+
+    it "cannot have a quality greater than 50" do
+      items = [Item.new("Aged Brie", 1, 50)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 50
+    end
+
+    it "Sulfuras sell in and quality values do not decrease" do
+      items = [Item.new("Sulfuras", 0, 80)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 0
+      expect(items[0].quality).to eq 80
+    end
+
+    # it "increases the quality value for Backstage passes" do
+    #   items = [Item.new("Backstage passes", 20, 2)]
+    #   GildedRose.new(items).update_quality()
+    #   expect(items[0].quality).to eq 3
+    # end
+
+    # it "increases the quality value by 2 for Backstage passes with <= 10 days left" do
+    #   items = [Item.new("Backstage passes", 9, 2)]
+    #   GildedRose.new(items).update_quality()
+    #   expect(items[0].quality).to eq 4
+    # end
+
+    # it "increases the quality value by 3 for Backstage passes with <= 5 days left" do
+    #   items = [Item.new("Backstage passes", 5, 2)]
+    #   GildedRose.new(items).update_quality()
+    #   expect(items[0].quality).to eq 5
+    # end
+
+    it "drops the quality of Backstage passes with < 0 days left to 0" do
+      items = [Item.new("Backstage passes", 0, 2)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+    end
+
+    # it "Conjured items drop in quality by 2" do
+    #   items = [Item.new("Conjured", 1, 2)]
+    #   GildedRose.new(items).update_quality()
+    #   expect(items[0].quality).to eq 0
+    # end
   end
 end
 
